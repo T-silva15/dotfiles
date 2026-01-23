be170ad3d9b8161cee1dd2a6f6fc7bbd550b0d7e
Tiago Silva <tiagoissilva@protonmail.com>
2026-01-23 10:22:55 +0000

    Add custom Tux penguin logo for fastfetch, simplify fish greeting

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,14 +1,3 @@
 function fish_greeting
-    echo ""
-    set_color cyan
-    echo '       .--.'
-    echo '      |o_o |'
-    echo '      |:_/ |'
-    echo '     //   \ \\'
-    echo '    (|     | )'
-    echo '   /`\_   _/`\\'
-    echo '   \___)=(___/'
-    set_color normal
-    echo ""
     fastfetch
 end
